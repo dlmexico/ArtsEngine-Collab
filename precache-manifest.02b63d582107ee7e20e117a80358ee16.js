16cd15a46f84992ef7fe086b9e3b90a98defa9c4
Rachel Leggett <rleggett@umich.edu>
2020-06-08 12:14:35 -0400

    Updates

---
+++ precache-manifest.02b63d582107ee7e20e117a80358ee16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e98e4400e99cb980176e6537e75d8b",
+    "url": "/faculty-collab-tool/index.html"
+  },
+  {
+    "revision": "2174627761b9cb53a24e",
+    "url": "/faculty-collab-tool/static/css/main.6efab67f.chunk.css"
+  },
+  {
+    "revision": "1e2ef728874624149dba",
+    "url": "/faculty-collab-tool/static/js/2.01a26d1f.chunk.js"
+  },
+  {
+    "revision": "7edc9d6a5411dd85ffa426b7d3cb6e20",
+    "url": "/faculty-collab-tool/static/js/2.01a26d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2174627761b9cb53a24e",
+    "url": "/faculty-collab-tool/static/js/main.c186a1d2.chunk.js"
+  },
+  {
+    "revision": "f6c508fdb5050b93705a",
+    "url": "/faculty-collab-tool/static/js/runtime-main.45a6ed17.js"
+  }
+]);
